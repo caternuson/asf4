08e62d3fc3def84606c0d5d477c14434fa157fa0
caternuson <caternuson@gmail.com>
2022-02-22 17:48:36 -0800

    add samd10

---
+++ samd10/driver_init.c
@@ -0,0 +1,233 @@
+/*
+ * Code generated from Atmel Start.
+ *
+ * This file will be overwritten when reconfiguring your Atmel Start project.
+ * Please copy examples or other code you want to keep to a separate file
+ * to avoid losing it when reconfiguring.
+ */
+
+#include "driver_init.h"
+#include <peripheral_clk_config.h>
+#include <utils.h>
+#include <hal_init.h>
+#include <hpl_gclk_base.h>
+#include <hpl_pm_base.h>
+
+#include <hpl_adc_base.h>
+#include <hpl_rtc_base.h>
+
+struct spi_m_sync_descriptor SPI_0;
+struct timer_descriptor      TIMER_0;
+
+struct adc_sync_descriptor ADC_0;
+
+struct i2c_m_sync_desc I2C_0;
+
+struct usart_sync_descriptor USART_0;
+
+struct pwm_descriptor PWM_0;
+
+struct dac_sync_descriptor DAC_0;
+
+void ADC_0_PORT_init(void)
+{
+}
+
+void ADC_0_CLOCK_init(void)
+{
+	_pm_enable_bus_clock(PM_BUS_APBC, ADC);
+	_gclk_enable_channel(ADC_GCLK_ID, CONF_GCLK_ADC_SRC);
+}
+
+void ADC_0_init(void)
+{
+	ADC_0_CLOCK_init();
+	ADC_0_PORT_init();
+	adc_sync_init(&ADC_0, ADC, (void *)NULL);
+}
+
+void SPI_0_PORT_init(void)
+{
+
+	gpio_set_pin_level(PA04,
+	                   // <y> Initial level
+	                   // <id> pad_initial_level
+	                   // <false"> Low
+	                   // <true"> High
+	                   false);
+
+	// Set pin direction to output
+	gpio_set_pin_direction(PA04, GPIO_DIRECTION_OUT);
+
+	gpio_set_pin_function(PA04, PINMUX_PA04D_SERCOM0_PAD0);
+
+	gpio_set_pin_level(PA05,
+	                   // <y> Initial level
+	                   // <id> pad_initial_level
+	                   // <false"> Low
+	                   // <true"> High
+	                   false);
+
+	// Set pin direction to output
+	gpio_set_pin_direction(PA05, GPIO_DIRECTION_OUT);
+
+	gpio_set_pin_function(PA05, PINMUX_PA05D_SERCOM0_PAD1);
+
+	// Set pin direction to input
+	gpio_set_pin_direction(PA06, GPIO_DIRECTION_IN);
+
+	gpio_set_pin_pull_mode(PA06,
+	                       // <y> Pull configuration
+	                       // <id> pad_pull_config
+	                       // <GPIO_PULL_OFF"> Off
+	                       // <GPIO_PULL_UP"> Pull-up
+	                       // <GPIO_PULL_DOWN"> Pull-down
+	                       GPIO_PULL_OFF);
+
+	gpio_set_pin_function(PA06, PINMUX_PA06D_SERCOM0_PAD2);
+}
+
+void SPI_0_CLOCK_init(void)
+{
+	_pm_enable_bus_clock(PM_BUS_APBC, SERCOM0);
+	_gclk_enable_channel(SERCOM0_GCLK_ID_CORE, CONF_GCLK_SERCOM0_CORE_SRC);
+}
+
+void SPI_0_init(void)
+{
+	SPI_0_CLOCK_init();
+	spi_m_sync_init(&SPI_0, SERCOM0);
+	SPI_0_PORT_init();
+}
+
+void I2C_0_PORT_init(void)
+{
+
+	gpio_set_pin_pull_mode(PA22,
+	                       // <y> Pull configuration
+	                       // <id> pad_pull_config
+	                       // <GPIO_PULL_OFF"> Off
+	                       // <GPIO_PULL_UP"> Pull-up
+	                       // <GPIO_PULL_DOWN"> Pull-down
+	                       GPIO_PULL_OFF);
+
+	gpio_set_pin_function(PA22, PINMUX_PA22C_SERCOM1_PAD0);
+
+	gpio_set_pin_pull_mode(PA23,
+	                       // <y> Pull configuration
+	                       // <id> pad_pull_config
+	                       // <GPIO_PULL_OFF"> Off
+	                       // <GPIO_PULL_UP"> Pull-up
+	                       // <GPIO_PULL_DOWN"> Pull-down
+	                       GPIO_PULL_OFF);
+
+	gpio_set_pin_function(PA23, PINMUX_PA23C_SERCOM1_PAD1);
+}
+
+void I2C_0_CLOCK_init(void)
+{
+	_pm_enable_bus_clock(PM_BUS_APBC, SERCOM1);
+	_gclk_enable_channel(SERCOM1_GCLK_ID_CORE, CONF_GCLK_SERCOM1_CORE_SRC);
+	_gclk_enable_channel(SERCOM1_GCLK_ID_SLOW, CONF_GCLK_SERCOM1_SLOW_SRC);
+}
+
+void I2C_0_init(void)
+{
+	I2C_0_CLOCK_init();
+	i2c_m_sync_init(&I2C_0, SERCOM1);
+	I2C_0_PORT_init();
+}
+
+void USART_0_PORT_init(void)
+{
+
+	gpio_set_pin_function(PA14, PINMUX_PA14D_SERCOM2_PAD0);
+
+	gpio_set_pin_function(PA15, PINMUX_PA15D_SERCOM2_PAD1);
+}
+
+void USART_0_CLOCK_init(void)
+{
+	_pm_enable_bus_clock(PM_BUS_APBC, SERCOM2);
+	_gclk_enable_channel(SERCOM2_GCLK_ID_CORE, CONF_GCLK_SERCOM2_CORE_SRC);
+}
+
+void USART_0_init(void)
+{
+	USART_0_CLOCK_init();
+	usart_sync_init(&USART_0, SERCOM2, (void *)NULL);
+	USART_0_PORT_init();
+}
+
+void delay_driver_init(void)
+{
+	delay_init(SysTick);
+}
+
+/**
+ * \brief Timer initialization function
+ *
+ * Enables Timer peripheral, clocks and initializes Timer driver
+ */
+static void TIMER_0_init(void)
+{
+	_pm_enable_bus_clock(PM_BUS_APBA, RTC);
+	_gclk_enable_channel(RTC_GCLK_ID, CONF_GCLK_RTC_SRC);
+	timer_init(&TIMER_0, RTC, _rtc_get_timer());
+}
+
+void PWM_0_PORT_init(void)
+{
+}
+
+void PWM_0_CLOCK_init(void)
+{
+	_pm_enable_bus_clock(PM_BUS_APBC, TC1);
+	_gclk_enable_channel(TC1_GCLK_ID, CONF_GCLK_TC1_SRC);
+}
+
+void PWM_0_init(void)
+{
+	PWM_0_CLOCK_init();
+	PWM_0_PORT_init();
+	pwm_init(&PWM_0, TC1, _tc_get_pwm());
+}
+
+void DAC_0_PORT_init(void)
+{
+}
+
+void DAC_0_CLOCK_init(void)
+{
+
+	_pm_enable_bus_clock(PM_BUS_APBC, DAC);
+	_gclk_enable_channel(DAC_GCLK_ID, CONF_GCLK_DAC_SRC);
+}
+
+void DAC_0_init(void)
+{
+	DAC_0_CLOCK_init();
+	dac_sync_init(&DAC_0, DAC);
+	DAC_0_PORT_init();
+}
+
+void system_init(void)
+{
+	init_mcu();
+
+	ADC_0_init();
+
+	SPI_0_init();
+
+	I2C_0_init();
+
+	USART_0_init();
+
+	delay_driver_init();
+
+	TIMER_0_init();
+
+	PWM_0_init();
+
+	DAC_0_init();
+}
